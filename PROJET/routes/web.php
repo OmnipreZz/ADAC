30483f7cedec4ace160b0855dada908b5c7c62e8
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-10-01 16:03:53 +0200

    routes for posts (CRUD)

--- PROJET/routes/web.php
+++ PROJET/routes/web.php
@@ -11,10 +11,26 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// home page
+Route::get('/', 'PostController@index' )->name('postIndex');
+
+// POST edit & update
+Route::get('/post/edit/{id}','PostController@edit')->name('postEdit');
+Route::post('/post/update/{id}','PostController@update')->name('postUpdate');
+
+// POST create & store 
+Route::get('post/create','PostController@create')->name('postCreate');
+Route::post('post/store','PostController@store')->name('postStore');
+
+// POST show
+Route::get('post/show/{id}','PostController@show')->name('postShow');
+
+// POST destroy
+Route::get('post/destroy/{id}','PostController@destroy')->name('postDestroy');
+
+
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
