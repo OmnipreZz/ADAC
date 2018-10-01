f661e713b9dfeb5ded541122ae898fa4ffdc2df7
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-10-01 14:42:42 +0200

    favorite model

--- PROJET/app/Favorite.php
+++ PROJET/app/Favorite.php
@@ -3,8 +3,29 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
 
 class Favorite extends Model
 {
-    //
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'post_id', 'user_id'
+    ];
+
+    public function post()
+    {
+    	return $this->hasOne('App\Post');
+    }
+
+    public function user()
+    {
+    	return $this->hasOne('App\User');
+    }
 }
