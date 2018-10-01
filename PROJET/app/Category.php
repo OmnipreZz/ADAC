34f4ba8d80df9e6ac317d9636082e93bcc07dfdf
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-10-01 14:42:11 +0200

    category model

--- PROJET/app/Category.php
+++ PROJET/app/Category.php
@@ -3,8 +3,30 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
 
 class Category extends Model
 {
-    //
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name'
+    ];
+
+    public function posts()
+    {
+    	return $this->hasMany('App\Post');
+    }
+
+    public function subcategories()
+    {
+    	return $this->hasMany('App\Subcategory');
+    }
+
 }
