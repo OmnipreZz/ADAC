d8b3b8981b7c801774e8f031ab8081eb38c744d1
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-10-01 14:43:24 +0200

    role model

--- PROJET/app/Role.php
+++ PROJET/app/Role.php
@@ -3,8 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
 
 class Role extends Model
 {
-    //
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name'
+    ];
+
+    public function users()
+    {
+    	return $this->hasMany('App\user');
+    }
 }
