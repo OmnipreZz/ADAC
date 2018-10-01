af50740902ca2df56ccfa8e76eaeb25d10515ea2
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-10-01 14:42:21 +0200

    comment model

--- PROJET/app/Comment.php
+++ PROJET/app/Comment.php
@@ -3,8 +3,29 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
 
 class Comment extends Model
 {
-    //
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'author', 'content'
+    ];
+
+    public function user()
+    {
+    	return $this->belongsTo('App\User');
+    }
+
+    public function post()
+    {
+    	return $this->belongsTo('App\Post');
+    }
 }
