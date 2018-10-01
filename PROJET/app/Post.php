ca756b1da5afa82cdf5842913f09daefd6f4b5d6
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-10-01 14:42:52 +0200

    post model

--- PROJET/app/Post.php
+++ PROJET/app/Post.php
@@ -18,4 +18,31 @@ class Post extends Model
     protected $fillable = [
         'title', 'author', 'content'
     ];
+
+    public function comments()
+    {
+    	return $this->hasMany('App\Comment');
+    }
+
+    public function category()
+    {
+    	return $this->belongsTo('App\Category');
+    }
+
+    public function user()
+    {
+    	return $this->belongsTo('App\User');
+    }
+
+    public function subcategories()
+    {
+    	return $this->belongsToMany('App\Subcategory');
+    }
+
+    public function favorites()
+    {
+    	return $this->belongsToMany('App\Favorite');
+    }
+
+    
 }
