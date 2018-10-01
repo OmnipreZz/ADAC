1851c2a57e60ebf8893882095afa33b5b80babd6
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-10-01 14:43:46 +0200

    subcategory model

--- PROJET/app/Subcategory.php
+++ PROJET/app/Subcategory.php
@@ -3,8 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
 
 class Subcategory extends Model
 {
-    //
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name'
+    ];
+
+    public function posts()
+    {
+    	return $this->belongsToMany('App\Post');
+    }
 }
