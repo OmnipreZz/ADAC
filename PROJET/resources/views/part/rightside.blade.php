96581d6fad30f3f17a5d36c6f2b00c598e33882e
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-10-07 23:42:30 +0200

    fix logout form

--- PROJET/resources/views/part/rightside.blade.php
+++ PROJET/resources/views/part/rightside.blade.php
@@ -18,6 +18,9 @@
                         onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                             {{ __('Logout') }}
                     </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                    </form>
                 </li>
             </ul>
     </div>
