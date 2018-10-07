6620cb051cf54409edfe728b9fa04cb818839def
xLeDocteurx <lenoirc.fr@gmail.com>
2018-10-07 15:46:36 +0200

    changed layout from users index

--- PROJET/resources/views/user/index.blade.php
+++ PROJET/resources/views/user/index.blade.php
@@ -9,9 +9,23 @@
 
                 <div class="card-body">
                 @auth
-                <ul>
+                <ul class="list-unstyled">
+
                     @foreach ($users as $user)
-                        <li>{{$user->id}} // <a href="{{route('user_show', $user->id)}}">{{$user->name}}</a></li>
+                    <li class="media border-bottom my-1">
+                    <span class="mx-3">{{$user->id}}</span>
+                        @if ($user->role_id == 1)
+                        <i class="mr-3 fas fa-crown" title="Role : Administrateur"></i>
+                        @elseif ($user->role_id == 2)
+                        <i class="mr-3 fas fa-chalkboard-teacher" title="Role : Professeur"></i>
+                        @elseif ($user->role_id == 3)
+                        <i class="mr-3 fas fa-user-graduate" title="Role : Eleve"></i>
+                        @endif
+
+                        <div class="media-body">
+                        <h5 class="mt-0 mb-1"><a href="{{route('user_show', $user->id)}}">{{$user->name}}</a></h5>
+                        </div>
+                    </li>
                     @endforeach
                 </ul>
                 @endauth
